f984c82bd47fb54d26069d28cc6d2e08614d8516
Praksh Gurung <prakashmeghgurung123@gmail.com>
2024-10-23 08:53:40 +0530

    update the design content

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,8 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import { Result } from "postcss";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
@@ -42,5 +44,39 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <div>
+      <div className="conttainer">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-center text-transparent">Real-world Results</p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center text-white/60 mt-4">See how I transformed concepts into engaging digital experiences.</p>
+        <div className="flex flex-col mt-10">
+          {portfolioProjects.map((project)=>(
+            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8">
+              <div className="">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+              </div>
+              <h3>{project.title}</h3>
+              <hr />
+              <ul>
+                {project.results.map((result)=> (
+                  <li key={result.title}>{result.title}</li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button>View Live Site</button>
+              </a>
+              <Image src={project.image} alt={project.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
